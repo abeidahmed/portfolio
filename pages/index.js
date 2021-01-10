dde64885006d76ae77bb89b5666796a171945af3
abeidahmed <abeidahmed92@gmail.com>
2021-01-10 22:38:42 +0530

    Add feeder-fish project section

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,100 @@
-export default function IndexPage() {
+import { Icon } from 'components/Icons';
+
+export default function Index() {
   return (
-    <div>
-      <h2>hello world</h2>
-    </div>
+    <main className="min-h-screen md:grid md:grid-cols-3 lg:grid-cols-12 md:gap-6">
+      <div className="flex items-center min-h-screen px-4 py-10 md:col-span-1 lg:col-span-5">
+        <div className="max-w-md md:ml-auto">
+          <h1 className="text-4xl font-medium text-gray-800">Abeid Ahmed</h1>
+          <p className="mt-2">
+            is building web-applications for more than 2 years with Ruby on Rails and React.
+          </p>
+          <div className="mt-5">
+            <a href="#" className="hover:underline">
+              See my resume
+            </a>
+          </div>
+          <div className="mt-10">
+            <h4 className="text-xl font-medium">Let's work together</h4>
+            <ul className="flex items-center mt-4 space-x-5">
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-700 transition duration-150 ease-in-out hover:text-gray-500"
+                >
+                  <Icon icon="mail" className="w-6 h-6" />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-700 transition duration-150 ease-in-out hover:text-gray-500"
+                >
+                  <Icon icon="linkedin" className="w-6 h-6" />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-700 transition duration-150 ease-in-out hover:text-gray-500"
+                >
+                  <Icon icon="twitter" className="w-6 h-6" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="md:col-span-2 lg:col-span-7">
+        <section className="flex flex-col justify-between min-h-screen px-4 text-yellow-900 bg-yellow-300">
+          <span className="flex-shrink-0 h-16" aria-hidden={true}></span>
+          <article className="flex items-center justify-center flex-1">
+            <div>
+              <div className="max-w-lg xl:max-w-xl md:text-center md:mx-auto">
+                <h2 className="text-3xl font-medium">Feeder fish</h2>
+                <p className="mt-2 xl:text-xl">
+                  Collect issues, ideas and compliments with a simple widget. Receive them as email.
+                  Dive deep with the dashboard.
+                </p>
+              </div>
+              <div className="max-w-2xl mx-auto mt-10">
+                <img
+                  src="/img/feeder-flow-mobile.png"
+                  alt="Screenshot of feeder fish"
+                  className="mx-auto sm:hidden"
+                />
+                <img
+                  src="/img/feeder-flow-large.png"
+                  alt="Screenshot of feeder fish"
+                  className="hidden sm:block"
+                />
+              </div>
+            </div>
+          </article>
+          <ul className="flex items-center justify-end py-5 space-x-5 lg:px-8 ">
+            <li>
+              <a href="#">
+                <Icon
+                  icon="github"
+                  type="stroke"
+                  className="w-6 h-6 transition duration-150 ease-in-out hover:opacity-75"
+                  strokeWidth={2}
+                />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <Icon
+                  icon="eye"
+                  type="stroke"
+                  className="w-6 h-6 transition duration-150 ease-in-out hover:opacity-75"
+                  strokeWidth={2}
+                />
+              </a>
+            </li>
+          </ul>
+        </section>
+      </div>
+    </main>
   );
 }
