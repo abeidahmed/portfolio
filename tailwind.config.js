d9b792112131d99db53814c880318142d00ea288
abeidahmed <abeidahmed92@gmail.com>
2021-01-11 00:19:33 +0530

    Update purge-css path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  purge: ['./components/**/*.js', './pages/**/*.js'],
+  purge: ['./components/**/*.js', './pages/**/*.js', './projects/**/*.js'],
   darkMode: false,
   theme: {
     extend: {
