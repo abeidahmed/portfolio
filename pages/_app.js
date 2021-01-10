52d8a8789571addd75b22e797dd4eff570c896f3
abeidahmed <abeidahmed92@gmail.com>
2021-01-10 21:26:39 +0530

    Add FuturaPT font

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/index.css'
+import '../styles/font.css';
+import '../styles/index.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
