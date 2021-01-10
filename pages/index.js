bcbec3d4acfaff9e417eb071ecd1969d72a74869
abeidahmed <abeidahmed92@gmail.com>
2021-01-10 22:59:11 +0530

    Add built with section

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,9 @@ import { Icon } from 'components/Icons';
 
 export default function Index() {
   return (
-    <main className="min-h-screen md:grid md:grid-cols-3 lg:grid-cols-12 md:gap-6">
-      <div className="flex items-center min-h-screen px-4 py-10 md:col-span-1 lg:col-span-5">
-        <div className="max-w-md md:ml-auto">
+    <main className="min-h-screen lg:grid lg:grid-cols-12 md:gap-6">
+      <div className="flex items-center min-h-screen px-4 py-10 sm:px-16 lg:col-span-5">
+        <div className="max-w-md lg:ml-auto">
           <h1 className="text-4xl font-medium text-gray-800">Abeid Ahmed</h1>
           <p className="mt-2">
             is building web-applications for more than 2 years with Ruby on Rails and React.
@@ -45,10 +45,10 @@ export default function Index() {
           </div>
         </div>
       </div>
-      <div className="md:col-span-2 lg:col-span-7">
-        <section className="flex flex-col justify-between min-h-screen px-4 text-yellow-900 bg-yellow-300">
+      <div className="lg:col-span-7">
+        <section className="flex flex-col justify-between min-h-screen px-4 text-yellow-900 bg-yellow-300 sm:px-16 lg:px-4">
           <span className="flex-shrink-0 h-16" aria-hidden={true}></span>
-          <article className="flex items-center justify-center flex-1">
+          <article className="flex items-center flex-1 md:justify-center">
             <div>
               <div className="max-w-lg xl:max-w-xl md:text-center md:mx-auto">
                 <h2 className="text-3xl font-medium">Feeder fish</h2>
@@ -71,28 +71,39 @@ export default function Index() {
               </div>
             </div>
           </article>
-          <ul className="flex items-center justify-end py-5 space-x-5 lg:px-8 ">
-            <li>
-              <a href="#">
-                <Icon
-                  icon="github"
-                  type="stroke"
-                  className="w-6 h-6 transition duration-150 ease-in-out hover:opacity-75"
-                  strokeWidth={2}
-                />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <Icon
-                  icon="eye"
-                  type="stroke"
-                  className="w-6 h-6 transition duration-150 ease-in-out hover:opacity-75"
-                  strokeWidth={2}
-                />
-              </a>
-            </li>
-          </ul>
+          <div className="py-5 sm:flex sm:items-end sm:justify-between xl:px-8">
+            <div>
+              <h6 className="font-medium">Built with:</h6>
+              <ul className="inline-flex flex-wrap items-center text-base">
+                <li className="mr-5 whitespace-nowrap">Rails</li>
+                <li className="mr-5 whitespace-nowrap">Vue.js</li>
+                <li className="mr-5 whitespace-nowrap">Postgresql</li>
+                <li className="mr-5 whitespace-nowrap">styled-components</li>
+              </ul>
+            </div>
+            <ul className="flex items-center justify-end mt-6 space-x-5 text-yellow-700 sm:justify-start sm:mt-0">
+              <li>
+                <a href="#">
+                  <Icon
+                    icon="github"
+                    type="stroke"
+                    className="w-5 h-5 transition duration-150 ease-in-out hover:opacity-75"
+                    strokeWidth={2}
+                  />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <Icon
+                    icon="eye"
+                    type="stroke"
+                    className="w-5 h-5 transition duration-150 ease-in-out hover:opacity-75"
+                    strokeWidth={2}
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
         </section>
       </div>
     </main>
