90be31c4f9bbe74bea83c5813ebf8e5a54a8194b
abeidahmed <abeidahmed92@gmail.com>
2021-01-11 00:09:41 +0530

    Segregate project images

--- pages/index.js
+++ pages/index.js
@@ -15,32 +15,12 @@ export default function Index() {
           images={
             <>
               <img
-                src="/img/feeder-flow-mobile.png"
+                src="/img/feederFish/feeder-flow-mobile.png"
                 alt="Screenshot of feeder fish"
                 className="mx-auto sm:hidden"
               />
               <img
-                src="/img/feeder-flow-large.png"
-                alt="Screenshot of feeder fish"
-                className="hidden sm:block"
-              />
-            </>
-          }
-          frameworks={['Rails', 'Vue.js', 'Postgresql', 'styled-components']}
-        />
-        <Project
-          classNames="text-yellow-900 bg-yellow-300"
-          title="Feeder fish"
-          description="Collect issues, ideas and compliments with a simple widget. Receive them as email. Dive deep with the dashboard."
-          images={
-            <>
-              <img
-                src="/img/feeder-flow-mobile.png"
-                alt="Screenshot of feeder fish"
-                className="mx-auto sm:hidden"
-              />
-              <img
-                src="/img/feeder-flow-large.png"
+                src="/img/feederFish/feeder-flow-large.png"
                 alt="Screenshot of feeder fish"
                 className="hidden sm:block"
               />
