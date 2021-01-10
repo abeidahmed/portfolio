a80c9d37d64951d581e745536bb9e9a99f408db5
abeidahmed <abeidahmed92@gmail.com>
2021-01-10 21:33:40 +0530

    Configure absolute imports

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import '../styles/font.css';
-import '../styles/index.css';
+import 'styles/font.css';
+import 'styles/index.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
