3d795e0af2e9c11216c0934f61ade3260555abad
abeidahmed <abeidahmed92@gmail.com>
2021-01-10 21:31:36 +0530

    Remove default nav component

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,7 @@
-import Nav from '../components/nav'
-
 export default function IndexPage() {
   return (
     <div>
-      <Nav />
-      <div className="py-20">
-        <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
-          Next.js + Tailwind CSS 2.0
-        </h1>
-      </div>
+      <h2>hello world</h2>
     </div>
-  )
+  );
 }
