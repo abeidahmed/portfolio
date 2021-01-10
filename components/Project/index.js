5e7462fea09c4a2a91d6bcb66050182fe79708a5
abeidahmed <abeidahmed92@gmail.com>
2021-01-11 00:04:43 +0530

    Make projects section scrollabel

---
+++ components/Project/index.js
@@ -0,0 +1,25 @@
+import { BuiltWith } from 'components/BuiltWith';
+import { Showcase } from 'components/Showcase';
+
+export function Project({ classNames, title, description, images, frameworks }) {
+  return (
+    <section
+      className={`flex flex-col justify-between min-h-screen px-4 text-yellow-900 bg-yellow-300 sm:px-16 lg:px-4 ${classNames}`}
+    >
+      <span className="flex-shrink-0 h-16" aria-hidden={true}></span>
+      <article className="flex items-center flex-1 md:justify-center">
+        <div className="flex-1">
+          <div className="max-w-lg xl:max-w-xl md:text-center md:mx-auto">
+            <h2 className="text-3xl font-medium">{title}</h2>
+            <p className="mt-2 xl:text-xl">{description}</p>
+          </div>
+          <div className="max-w-2xl mx-auto mt-10">{images}</div>
+        </div>
+      </article>
+      <div className="py-5 sm:flex sm:items-end sm:justify-between xl:px-8">
+        <BuiltWith frameworks={frameworks} />
+        <Showcase githubLink="#" liveLink="#" />
+      </div>
+    </section>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,53 @@
-import { BuiltWith } from 'components/BuiltWith';
-import { Showcase } from 'components/Showcase';
 import { Intro } from 'components/Intro';
+import { Project } from 'components/Project';
 
 export default function Index() {
   return (
-    <main className="min-h-screen lg:grid lg:grid-cols-12 md:gap-6">
+    <main className="min-h-screen lg:grid lg:grid-cols-12 lg:gap-6">
       <div className="flex items-center min-h-screen px-4 py-10 sm:px-16 lg:col-span-5">
         <Intro />
       </div>
-      <div className="lg:col-span-7">
-        <section className="flex flex-col justify-between min-h-screen px-4 text-yellow-900 bg-yellow-300 sm:px-16 lg:px-4">
-          <span className="flex-shrink-0 h-16" aria-hidden={true}></span>
-          <article className="flex items-center flex-1 md:justify-center">
-            <div className="flex-1">
-              <div className="max-w-lg xl:max-w-xl md:text-center md:mx-auto">
-                <h2 className="text-3xl font-medium">Feeder fish</h2>
-                <p className="mt-2 xl:text-xl">
-                  Collect issues, ideas and compliments with a simple widget. Receive them as email.
-                  Dive deep with the dashboard.
-                </p>
-              </div>
-              <div className="max-w-2xl mx-auto mt-10">
-                <img
-                  src="/img/feeder-flow-mobile.png"
-                  alt="Screenshot of feeder fish"
-                  className="mx-auto sm:hidden"
-                />
-                <img
-                  src="/img/feeder-flow-large.png"
-                  alt="Screenshot of feeder fish"
-                  className="hidden sm:block"
-                />
-              </div>
-            </div>
-          </article>
-          <div className="py-5 sm:flex sm:items-end sm:justify-between xl:px-8">
-            <BuiltWith frameworks={['Rails', 'Vue.js', 'Postgresql', 'styled-components']} />
-            <Showcase githubLink="#" liveLink="#" />
-          </div>
-        </section>
+      <div className="lg:col-span-7 lg:overflow-y-auto lg:flex lg:flex-col lg:h-screen">
+        <Project
+          classNames="text-yellow-900 bg-yellow-300"
+          title="Feeder fish"
+          description="Collect issues, ideas and compliments with a simple widget. Receive them as email. Dive deep with the dashboard."
+          images={
+            <>
+              <img
+                src="/img/feeder-flow-mobile.png"
+                alt="Screenshot of feeder fish"
+                className="mx-auto sm:hidden"
+              />
+              <img
+                src="/img/feeder-flow-large.png"
+                alt="Screenshot of feeder fish"
+                className="hidden sm:block"
+              />
+            </>
+          }
+          frameworks={['Rails', 'Vue.js', 'Postgresql', 'styled-components']}
+        />
+        <Project
+          classNames="text-yellow-900 bg-yellow-300"
+          title="Feeder fish"
+          description="Collect issues, ideas and compliments with a simple widget. Receive them as email. Dive deep with the dashboard."
+          images={
+            <>
+              <img
+                src="/img/feeder-flow-mobile.png"
+                alt="Screenshot of feeder fish"
+                className="mx-auto sm:hidden"
+              />
+              <img
+                src="/img/feeder-flow-large.png"
+                alt="Screenshot of feeder fish"
+                className="hidden sm:block"
+              />
+            </>
+          }
+          frameworks={['Rails', 'Vue.js', 'Postgresql', 'styled-components']}
+        />
       </div>
     </main>
   );
