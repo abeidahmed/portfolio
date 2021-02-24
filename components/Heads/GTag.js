11d1153dd2310a1623a97c719625f967361ad662
abeidahmed <abeidahmed92@gmail.com>
2021-02-24 17:16:09 +0530

    Set GTAG

--- components/Heads/GTag.js
+++ components/Heads/GTag.js
@@ -1,14 +1,19 @@
+const GTAG = 'G-ZJDJ71X3F0';
+
 export function GTag() {
   return (
     <>
-      <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTAG}`} />
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${GTAG}`}
+      />
       <script
         dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.GTAG}', {
+            gtag('config', '${GTAG}', {
               page_path: window.location.pathname,
             });
           `,
